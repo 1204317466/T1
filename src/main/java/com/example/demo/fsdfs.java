7f870bfe4a7d05ca47b358deafef86a569aa0e36
shenhao <1204317466@qq.com>
2020-06-02 17:46:26 +0800

    Merge branch 'master' of https://github.com/1204317466/T1
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- src/main/java/com/example/demo/fsdfs.java
+++ src/main/java/com/example/demo/fsdfs.java
@@ -1,8 +1,17 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Author Shen Hao
  * Created by Shen Hao on 2020/6/2
  */
+@RestController
 public class fsdfs {
+
+    @RequestMapping("/test")
+    public String ttt(){
+        return "jkjkjkj";
+    }
 }
